ff1712818e8a4e3cbfb29370d7e6ddc0b7288eeb
kev402 <ke43vn74@gmail.com>
2025-04-18 12:38:59 -0500

    Update bromita.py

--- bromita.py
+++ bromita.py
@@ -1,26 +1,26 @@
-# Puedes cambiar el nombre del archivo a algo más serio para que sea más convicente y no hayan sospechas de la broma.
+# Puedes cambiar el nombre del archivo a algo más serio para que sea más convincente y no hayan sospechas de la broma.
 
-import os
+import subprocess as sp
 import time
 
 print("Bienvenido a la actualización de tu consola")
-solicitud = input("Desea actualizar su consola? (s/n): ")
-if solicitud == "S" or solicitud == "s":
+solicitud = input("Desea actualizar su consola? (s/n): ").strip().lower()
+if solicitud == "s":
     time.sleep(3)
     print("Descargando colores...")
     time.sleep(3)
     print("Aplicando color...")
     time.sleep(3)
-    os.system("color a")
+    sp.run("color a", shell=True)
     time.sleep(3)
     print("Aplicando nuevo banner...")
     time.sleep(3)
     print("CAÍSTE... XD")
     time.sleep(2)
-    os.system("curl ascii.live/rick")
+    sp.run(["curl", "ascii.live/rick"])
 
 else:
     print("Lastima, ahora a tu sabrás las consecuencias...")
-    os.system("color a")
+    sp.run("color a", shell=True)
     time.sleep(3)
-    os.system("curl ascii.live/rick")
+    sp.run(["curl", "ascii.live/rick"])
